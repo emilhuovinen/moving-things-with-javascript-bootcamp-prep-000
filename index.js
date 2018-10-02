e8773f25d8fc09679b7f9b11b12546204d592cf4
emil.huo <emil.huo@gmail.com>
2018-10-02 13:34:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger');
 
-function moveDodgerLeft() {
+function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
  
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left + 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
+  if (e.which === 39) {
+    moveDodgerRight()
   }
 })
